0508aa609c012b4f2d9391258755b344cda7695a
NeoDau <neodau@gmail.com>
2022-09-27 08:52:36 -0700

    6-size.c.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 		*float
 		*/
 
-		printf("Size of a %lu Byte(s)\n" sizeof(char));
-		printf("Size of a %lu Byte(s)\n" sizeof(int));
-		printf("Size of a %lu Byte(s)\n" sizeof(long int));
-		printf("Size of a %lu Byte(s)\n" sizeof(long long int));
-		printf("Size of a %lu Byte(s)\n" sizeof(float));
+		printf("Size of a %lu Byte(s)\n", sizeof(char));
+		printf("Size of a %lu Byte(s)\n", sizeof(int));
+		printf("Size of a %lu Byte(s)\n", sizeof(long int));
+		printf("Size of a %lu Byte(s)\n", sizeof(long long int));
+		printf("Size of a %lu Byte(s)\n", sizeof(float));
 		return (0);
 	}
