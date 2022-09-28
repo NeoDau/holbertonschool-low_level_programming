2adcba0b13b2e0568d2f1fdb52570980641f6385
NeoDau <neodau@gmail.com>
2022-09-28 10:43:39 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	putchar(base);
+	putchar('\n');
+	return (0);
+}
