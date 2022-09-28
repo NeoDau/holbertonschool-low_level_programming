0cbfef26ca21cb78989087bc8c5e13501a60d8b7
NeoDau <neodau@gmail.com>
2022-09-28 10:28:09 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0; base <= 10; base++)
+	printf(base);
+	printf('\n');
+	return (0);
+}
