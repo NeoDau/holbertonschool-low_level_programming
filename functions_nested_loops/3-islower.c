712f736eafcb48edfb9f04d4b648ea3e1bc365f7
NeoDau <neodau@gmail.com>
2022-09-29 10:41:24 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *islower - Shows 1 if the input is a
+ *@c: Another cases, shows 0
+ *
+ * Return: Always 0
+ */
+
+int islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
