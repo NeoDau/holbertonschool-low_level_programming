f6e9400bd60bddc135601ab40f6d315f2fa17577
NeoDau <neodau@gmail.com>
2022-09-30 05:15:06 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isalpha - function that checks for alphabetic character
+ *
+ *Return: Always 0
+ */
+
+int _asalpha(int c)
+{
+	if (c >= 'a'; c <= 'z'; c >= 'A'; c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
