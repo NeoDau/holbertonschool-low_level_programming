40ed4eb186c6b9923ba740d935d2bc6591fb7ce7
NeoDau <neodau@gmail.com>
2022-09-30 11:36:15 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *time_table - that prints the 9 times table
+ *
+ */
+
+void time_table(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			int x = num1 * num2;
+
+			if (num1 == 0)
+			{
+				putchar('0');
+			}
+			else if (x <= 9)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(x + '0');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(x / 10 + '0');
+				putchar(x % 10 + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
