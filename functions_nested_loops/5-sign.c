25197154d478b26f6c91294f8b0ceaaed9132ad0
NeoDau <neodau@gmail.com>
2022-09-30 05:51:05 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_sign - nes if the input number greater, iqual or less than zero.m
+ *@n: the input namber
+ *
+ *Return: 1 is greater than zero. 0 is zero. -1 is less than zero.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < -1)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+
+	}
+
+}
