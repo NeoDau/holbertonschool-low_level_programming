412e665e7ffd4fa82453dd4649617572754c6def
NeoDau <neodau@gmail.com>
2022-10-03 16:24:39 -0700

    3-

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - function that numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n <= '9')
+		{
+			putchar(n);
+		}
+	}
+}
