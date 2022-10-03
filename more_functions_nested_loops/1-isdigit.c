b34ebd08923826cb3ad135bb2ca59f6f4cc7c460
NeoDau <neodau@gmail.com>
2022-10-03 10:47:35 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_insdigit - digit 0 & 9
+ *@c: value variable.
+ *
+ *Return: 1 & 0
+ */
+
+int _insdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
