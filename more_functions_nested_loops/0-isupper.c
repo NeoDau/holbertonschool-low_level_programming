2e380a67573e875fb3e67372ee5bf3f348114663
NeoDau <neodau@gmail.com>
2022-10-03 10:29:16 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_insupper - Uppercaste
+ *
+ *Return: 1 & 0
+ */
+
+int _insupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
