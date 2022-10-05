1ad630d05d6a4039946eff30963dc2d7843213b3
NeoDau <neodau@gmail.com>
2022-10-05 06:29:42 -0700

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,26 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-		{
-			printf("FizzBuzz ");
-		}
+		
+			printf("FizzBuzz");
+		
 		else if (num % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+		
+			printf("Buzz");
+		
 		else if (num % 3 == 0)
-		{
-			printf("Fizz ");
-		}
+		
+			printf("Fizz");
+		
 		else
-		{
+		
 			printf("%d ", num);
-		}
+		
+		if ( num == 100)
+			continue;	
+		printf(" ");
+		
 	}
 	printf("\n");
-
 	return (0);
 }
