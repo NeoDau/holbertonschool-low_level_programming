b251de75521ae76682587f7aa503b5fa97645747
NeoDau <neodau@gmail.com>
2022-10-05 09:12:52 -0700

    10-p

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,6 +8,7 @@ void print_triangle(int size)
 {
 	int n;
 	int e;
+	int o;
 
 	if (size <= 0)
 	{
@@ -16,6 +17,10 @@ void print_triangle(int size)
 	for (n = 0; n < size; n++)
 	{
 		for (e = size - n; e > 1; e--)
+		{
+			putchar(' ');
+		}
+		for (o = n - e; o > 1; o--)
 		{
 			putchar('#');
 		}
