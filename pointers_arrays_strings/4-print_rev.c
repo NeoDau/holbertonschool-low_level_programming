eb3943687be4510866e5c17b804ef230643cc617
NeoDau <neodau@gmail.com>
2022-10-06 09:15:14 -0700

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_rev - function in reverse
+ *@s: char
+ *Return: Always - 0
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (a = a - 1; a >= 0; a--)
+		{
+			putchar(s[a]);
+			putchar('\n');
+		}
+	}
+}
