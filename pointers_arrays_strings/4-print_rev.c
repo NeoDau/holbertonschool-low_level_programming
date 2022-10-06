94c2d66809b80ed55bfebfa17d1d639014b2c95f
NeoDau <neodau@gmail.com>
2022-10-06 09:36:52 -0700

    4-

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,9 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = a - 1; a >= 0; a--)
 	{
-		for (a = a - 1; a >= 0; a--)
-		{
-			putchar(s[a]);
-		}
-		putchar('\n');
+		putchar(s[a]);
 	}
+	putchar('\n');
 }
