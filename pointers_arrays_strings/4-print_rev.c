869af1b9e9f822967d37d4cae3bd611bdf6e9681
NeoDau <neodau@gmail.com>
2022-10-06 10:05:50 -0700

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  *print_rev - function in reverse
  *@s: char
@@ -8,7 +9,7 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = strlen(s) - 1; a >= 0; a--)
+	for (a = _strlen(s) - 1; a >= 0; a--)
 	{
 		putchar(s[a]);
 	}
