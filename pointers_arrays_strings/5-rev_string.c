7ec8b0279dba5024e6e639bf6c31513380781ad5
NeoDau <neodau@gmail.com>
2022-10-06 18:43:45 -0700

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *rev_string - function
+ *@s: value
+ *
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	int y;
+	int a;
+
+	y = strlen(s);
+
+	for (x = 0; x < y / 2; x++)
+	{
+		a = s[x];
+		s[x] = s[y - x - 1];
+		s[y - x - 1] = a;
+	}
+}
