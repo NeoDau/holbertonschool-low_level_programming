07f8c4a6c398e68006dbe4098c53f073ab48aa50
NeoDau <neodau@gmail.com>
2022-10-10 12:21:10 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,16 +3,14 @@
  *_strcmp - function two strings
  *@s1: char
  *@s2: char
- *Return: s1[a] & s2[a]
+ *Return: s1[a] & s2[a].
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int a;
 
-	a = 0;
-
-	for (s1[a] - s2[a] == 0 && s1[a] != '\0'; a++)
+	for (a 0 0; s1[a] - s2[a] == 0 && s1[a] != '\0'; a++)
 	{}
 	return (s1[a] - s2[a]);
 }
