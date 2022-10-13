3202c569aa66971b2f364b53e3cae1e27e4084da
NeoDau <neodau@gmail.com>
2022-10-13 11:01:10 -0700

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,23 +11,22 @@ char *_strstr(char *haystack, char *needle)
 	int a;
 	int b;
 
-	for (a = 0; haystack[a] != '\0'; a++)
-	{
-		for (b = 0; needle[b] != '\0'; b++)
+	for (b = 0; needle[b] != '\0'; b++)
+	{}
+		for (a = 0; haystack[a] != '\0'; a++)
 		{
 			if (needle[b] != haystack[a])
 			{
 				break;
 			}
 		}
-/**		if (a != b)
+		if (a != b)
 		{
 			haystack++;
 		}
 		else
 		{
 			return (haystack);
-		}*/
-	}
+		}
 	return (NULL);
 }
