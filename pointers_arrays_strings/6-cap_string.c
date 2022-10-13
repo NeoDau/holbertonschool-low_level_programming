f85cc7f0cedbad1790c99db90c846bd049a2a5fc
NeoDau <neodau@gmail.com>
2022-10-13 10:25:34 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,29 @@
 #include "main.h"
-#include <ctype.h>
-#include <stdio.h>
-#include "2-strlen"
 /**
  *cap_string - function
  *@cap: value char
  *Return: cap
  */
 
-char *cap_string(char *cap)
+char *cap_string(char *c)
 {
 	int a;
 
-	for (a = 0; cap[a] != '\0'; a++)
+	for (a = 0; c[a] != '\0'; a++)
 	{
-		if (cap[a] == ' ' || cap[a] == '\t' || cap[a] == '\n' ||
-			cap[a] == ',' || cap[a] == ';' || cap[a] == '.' ||
-			cap[a] == '!' || cap[a] == '?' || cap[a] == '"' ||
-			cap[a] == '(' || cap[a] == ')' || cap[a] == '{' ||
-			cap[a] == '}' || a == 0)
+		if ('a' <= c[a] && c[a] <= 'z')
 		{
-			if (cap[a + 1] >= 'a' && cap[a + 1] <= 'z')
-				cap[a + 1] = cap[a + 1] - 32;
-
-		}
-		else if (cap[a] = _strlen(0))
-		{
-			cap[a] = cap[a] - 32;
+			if (c[a - 1] == ' ' || c[a - 1] == ',' ||
+					c[a - 1] == ';' || c[a - 1] == '.' ||
+				c[a - 1] == '!' || c[a - 1] == '?' ||
+				c[a - 1] == '\"' || c[a - 1] == '(' ||
+				c[a - 1] == ')' || c[a] == '{' ||
+				c[a - 1] == '}' || c[a - 1] == '	' ||
+				c[a - 1] == '\n' || a == 0)
+			{
+				c[a] = (c[a] - 32);
+			}
 		}
 	}
-	return (cap);
+	return (c);
 }
