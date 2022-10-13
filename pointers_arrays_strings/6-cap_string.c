c73fe053db20dd248470467553a391033f56503c
NeoDau <neodau@gmail.com>
2022-10-13 10:13:28 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 #include <stdio.h>
+#include "2-strlen"
 /**
  *cap_string - function
  *@cap: value char
@@ -17,11 +18,15 @@ char *cap_string(char *cap)
 			cap[a] == ',' || cap[a] == ';' || cap[a] == '.' ||
 			cap[a] == '!' || cap[a] == '?' || cap[a] == '"' ||
 			cap[a] == '(' || cap[a] == ')' || cap[a] == '{' ||
-			cap[a] == '}')
+			cap[a] == '}' || a == 0)
 		{
 			if (cap[a + 1] >= 'a' && cap[a + 1] <= 'z')
 				cap[a + 1] = cap[a + 1] - 32;
 
+		}
+		else if (cap[a] = _strlen(0))
+		{
+			cap[a] = cap[a] - 32;
 		}
 	}
 	return (cap);
