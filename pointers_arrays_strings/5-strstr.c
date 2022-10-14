fb3f3043343107d727b95e57bd5d61cd82443487
NeoDau <neodau@gmail.com>
2022-10-13 19:36:16 -0700

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,48 @@
 #include "main.h"
+int _strncmp(char *s1, char *s2, int n);
 /**
  *_strstr - function
  *@haystack: longer string
  *@needle: substring
  *Return: null
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int a;
-	int b;
-
-	for (b = 0; needle[b] != '\0'; b++)
-	{}
-		for (a = 0; haystack[a] != '\0'; a++)
-		{
-			if (needle[b] != haystack[a])
-			{
-				break;
-			}
-		}
-		if (a != b)
-		{
-			haystack++;
-		}
-		else
-		{
-			return (haystack);
-		}
+	char *aux = haystack;
+	for (; *aux; aux++)
+	{
+		if (_strncmp(aux, needle, _strlen(needle)) == 0)
+			return (aux);
+	}
 	return (NULL);
+}
+/**
+ *_strcmp - function two strings
+ *@s1: char
+ *@s2: char
+ *Return: s1[a] & s2[a].
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	int a = 0;
+
+	for (a = 0; a < n && s1[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+	}
+	return (0);
+}
+/**
+ *_strlen - returns the length
+ *@s: char
+ *Return: 0
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
 }
