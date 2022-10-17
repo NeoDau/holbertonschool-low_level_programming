20ccf21b6c1d7b157b22476ff9bc59e7cc7cc39a
NeoDau <neodau@gmail.com>
2022-10-17 05:59:33 -0700

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_pow_recursion - function return value of x
+ *@x: value 
+ *@y: raise to the power
+ *Return: x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (x);
+}
