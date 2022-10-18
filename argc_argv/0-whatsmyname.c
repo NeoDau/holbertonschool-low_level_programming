750c3b351ab5c9f9486174232af9e6017bfbd3b3
NeoDau <neodau@gmail.com>
2022-10-18 05:58:32 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ *@argc: int
+ *@argv: char
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
