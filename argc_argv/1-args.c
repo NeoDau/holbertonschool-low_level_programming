9898455cc416ee0bc93745bea9dd827e0877478f
NeoDau <neodau@gmail.com>
2022-10-18 06:30:18 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function prints the number
+ *@argc: int
+ *@argv: char
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);	
+}
