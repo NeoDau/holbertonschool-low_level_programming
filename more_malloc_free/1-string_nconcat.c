84dbecbf1040b3c47cbbdbee621846e9551bee57
NeoDau <neodau@gmail.com>
2022-10-24 08:31:23 -0700

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int a;
+/**	int a;
 	unsigned int b;
 	char *pr;
 
@@ -23,6 +23,41 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (a = 0; s1[a] != '\0'; a++)
 	{}
 	pr = malloc(sizeof(char) * (a + n + 1));
+	if (pr == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		pr[a] = s1[a];
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+		pr[a] = s2[b];
+		a++;
+	}
+	pr[a] = '\0';
+	return (pr);
+}*/
+{
+	char *pr;
+	int a;
+	int b;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (a = 0; s1[a] != '\0'; a++)
+	{}
+	for (b = 0; s2[b] != '\0'; b++)
+	{}
+	pr = malloc(sizeof(char) * (a + b + 1));
+
 	if (pr == NULL)
 	{
 		return (NULL);
