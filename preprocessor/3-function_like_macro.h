f54cfc20775819fb2fb7d6f8cc6ff7ab66f859dc
NeoDau <neodau@gmail.com>
2022-10-25 08:41:46 -0700

    3-function_like_macro.h

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_function_like_macro_h_
-#define _3_function_like_macro_h_
-#define abs(x) (x >= 0 ? (x) : (-x))
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+#define ABS(x) (x >= 0 ? (x) : (-x))
 #endif
