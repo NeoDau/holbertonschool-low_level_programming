afd8ab2dd1187f375156bed94baaa6a12cd19b31
NeoDau <neodau@gmail.com>
2022-10-27 08:34:46 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,13 @@
  *@age: age dog
  *@owner: owner dog
  */
-typedef struct dog
+struct dog
 {
-        *char name;
+        char *name;
         float age;
-        *char owner;
+        char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_0;
