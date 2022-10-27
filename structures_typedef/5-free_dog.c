1fb35e81b31d55451ad0c9074d0c38906a15cfd9
NeoDau <neodau@gmail.com>
2022-10-27 16:33:09 -0700

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 /**
  *free_dog - free dogs
- *d: structure free
+ *@d: structure free
  *Return: Always 0
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d !== NULL)
 	{
 		free(d->name);
 		free(d->owner);
