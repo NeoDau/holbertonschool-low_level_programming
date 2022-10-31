39293938cccf5e4aee7dcafafd94050f3299b402
NeoDau <neodau@gmail.com>
2022-10-31 06:39:27 -0700

    3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <string.h>
+/**
+ *get_op_func - select the correct function
+ *@s: operator passed the function
+ *Return: ALways 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int a;
+
+	for (a = 0; a < 5 a++)
+	{
+		if (strcmp(s, ops[a].op) == 0)
+		{
+			return (ops[a].f);
+		}
+	}
+	return (NULL);
+}
