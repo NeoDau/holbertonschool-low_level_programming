7c98bd29ade289f823678baad6e01fdba1976ecc
NeoDau <neodau@gmail.com>
2022-11-15 07:18:54 -0800

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_list - print all the elements os fe linked list
+ *@h: pointer to de list
+ *Return: Number of node
+ */
+size_t print_list(const list_t *h)
+{
+	int a = 0;
+
+	for (; h; h = h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%u %s\n", h->len, h->str);
+		}
+		a++;
+	}
+	return (a);
+}
