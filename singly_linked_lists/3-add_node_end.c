76ac05597e0f45d62cf752c86c920ebad2334d28
NeoDau <neodau@gmail.com>
2022-11-16 08:59:12 -0800

    3-add_node_end.c

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ *add_node_end - function add new node at the end list
+ *@head:doble pointer to list
+ *@str: string to add a node
+ *Return: endNode
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *endNodo;
+	int a;
+	list_t *ptr = *head;
+
+	endNodo = malloc(sizeof(list_t));
+	if (!endNodo)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = endNodo;
+		return (endNodo);
+	}
+	if (ptr)
+	{
+		for (; ptr->next; ptr = ptr->next)
+		{}
+	}
+	for (a = 0; str[a]; a++)
+	{}
+
+	endNodo->str = strdup(str);
+	endNodo->len = a;
+/**	endNodo->next = NULL;*/
+	ptr->next = endNodo;
+
+	return (endNodo);
+}
