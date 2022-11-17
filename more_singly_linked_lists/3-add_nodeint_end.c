a3e946524fca5f9613dc262b299c5e38b8c01c54
NeoDau <neodau@gmail.com>
2022-11-17 15:07:39 -0800

    3-add_nodeint_end.c

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ *add_nodeint_end - function add node end list
+ *@head: pointer to list
+ *@n int to add node
+ *Return: endNodo
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr = *head;
+	listint_t *endNodo;
+
+	endNodo = malloc(sizeof(listint_t));
+
+	if (!endNodo)
+	{
+		return (NULL);
+	}
+	endNodo->n = n;
+	endNodo->next = NULL;
+	if (*head == NULL)
+	{
+		*head = endNodo;
+		return (endNodo);
+	}
+
+	for (; ptr->next; ptr = ptr->next)
+	{}
+	ptr->next = endNodo;
+
+	return (endNodo);
+}
