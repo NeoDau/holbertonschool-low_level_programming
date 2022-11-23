09a8346d3b07105cfe237a0526fdc195d554360d
NeoDau <neodau@gmail.com>
2022-11-22 17:37:32 -0800

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
-		return (1);
+		return (-1);
 	}
 	wr = write(fd, text_content, len);
 	if (wr == -1)
