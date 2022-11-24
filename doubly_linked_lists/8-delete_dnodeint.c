782607e0dde87ff3ffac4d253a3c719f75451489
NeoDau <neodau@gmail.com>
2022-11-24 15:21:19 -0800

    8-delete_dnodeint.c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+/**
+ *dlistint_len - alternative function that returns the number of nodes
+ *@h: pointer to de list
+ *Return: number of node
+ */
+size_t dlistint_len(const dlistint_*h)
+{
+	size_t node = 0;
+
+	if (!h)
+	{
+		return (0);
+	}
+	for (; h; h = h->next)
+	{
+		node++;
+	}
+	return (node);
+}
+/**
+ *delete_dnodeint_at_index - deleete node in a dobly linked list
+ *@headd: double pointer to the list
+ *@index: index of the node to delete
+ *Return: 1success, -1 fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+		dlistint_t *ptr = *head;
+		unsigned int a = 0;
+
+		if (*head == NULL)
+		{
+			return (-1)
+		}
+		if (index == NULL)
+		{
+			(*head) = ptr->next;
+			if (ptr->next)
+			{
+				ptr->next->prev = NULL;
+			}
+			ptr->next = NULL;
+			free(ptr);
+			return (1)
+		}
+		for (; a < index; ptr = ptr->next)
+		{
+			a++;
+		}
+		ptr->prev->next = ptr->next;
+		if (ptr->next)
+		{
+			ptr->next->prev = ptr->prev;
+		}
+		free(ptr);
+		return (1);
+
+}
